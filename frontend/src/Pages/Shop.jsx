d5e44b0ccb7f3566f7424a9b388d444af849c81d
Nityanutan mishra <mishra.two1@gmail.com>
2026-02-17 15:46:17 +0530

    fix newsletter path case

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -2,7 +2,7 @@ import Popular from "../Components/Popular/Popular"
 import Hero from "../Components/Hero/Hero"
 import Offers from "../Components/Offers/Offers"
 import NewCollections from "../Components/NewCollections/NewCollections"
-import Newsletter from "../Components/Newsletter/NewsLetter.jsx"
+import Newsletter from "../Components/NewsLetter/Newsletter.jsx"
 import Footer from "../Components/Footer/Footer"
 
 
